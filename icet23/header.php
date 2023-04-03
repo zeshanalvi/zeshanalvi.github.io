6f1dda5213d3b4b46d3569cf37b6c96decbc82bb
Zeshan Khan <zeshankhanalvi@gmail.com>
2023-04-03 11:23:41 +0500

    page name changed

--- icet23/header.php
+++ icet23/header.php
@@ -1,40 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./Styles/index.css" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans%3A700" />
-    <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css?family=Montserrat%3A300%2C400%2C500%2C700%2C800%2C900" />
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css?family=Source+Sans+Pro%3A300%2C400%2C500%2C700%2C800%2C900" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-    <script src="index.js"></script>
-    <title>Document</title>
-</head>
 
-<body>
     <nav>
       <a  href="#home"><img src="./assets/ICET-02-02.png" alt="Logo Image" width="180" ></a>
       <div class="line-1">
-
       </div>
-
       <div class="Fast-logo">
         <img src="./assets/nationaluniversityofcomputerandemergingscienceslogo-1-tVe.png">
         <br>
         <h5>29 - 30 NOVEMBER 2023 <br>INDUSTRIAL ESTATE</h5>
-      
       </div>
       <a class="btn-a"><button class="ms-auto">Registered Now</button></a>
-
       <div class="line-2"></div>
     </nav>
-      <br>      
+      <br>
       <div class="nav-link">
         <a href="#programme">PROGRAMME</a>
         <a href="#authors">FOR AUTHORS</a>
