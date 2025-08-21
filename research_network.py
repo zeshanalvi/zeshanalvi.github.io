f39c1f698c79abb5b174104b714b592c642431b5
zeshanalvi <zeshankhanalvi@gmail.com>
2025-08-21 15:05:24 +0500

    Updates in the co-author network representation

--- research_network.py
+++ research_network.py
@@ -7,6 +7,13 @@ from collections import defaultdict
 class Scholarnet:
     def __init__(self,dblp_url="https://dblp.org/pid/232/1606.html"):
         self.dblp_url=dblp_url
+        self.graph=nx.Graph()
+    def author_short(self,full_name):
+        first_letters = ""
+        for word in full_name.split():
+            if word:
+                first_letters+=word[0]
+        return first_letters
 
     def extract_graph(self):
         response = requests.get(self.dblp_url)
@@ -25,49 +32,42 @@ class Scholarnet:
                 for j in range(i+1, len(authors)):
                     pair = tuple(sorted([authors[i], authors[j]]))
                     edge_weights[pair] += 1
-        G = nx.Graph()
         for author, count in author_papers.items():
             if(author in main_author):
-                G.add_node(author, size=count*10, color="red")
+                self.graph.add_node(author,label=self.author_short(author),title=f"{author}\nTotal Papers: {count}", size=10+count*2, color="red")
             else:
-                G.add_node(author, size=count*10, color="#1f78b4")
+                self.graph.add_node(author,label=self.author_short(author),title=f"{author}\nPapers with me: {count}", size=10+count*2, color="#1f78b4")
         for (a, b), w in edge_weights.items():
-            G.add_edge(a, b, weight=w*3, color="#B1C3EB2C")
-        return G
-
-
-net=Scholarnet(dblp_url="https://dblp.org/pid/290/7993.html")
-net=Scholarnet()
-G=net.extract_graph()
-
-
+            self.graph.add_edge(a, b, weight=w*3, color="#B1C3EB2C")
+        
+    def show(self,height="600px",width="100%",bg_color="#ECE9E9",font_color="#636060"):
+        # Create Pyvis network
+        net = Network(notebook=True, height=height, width=width, bgcolor=bg_color, font_color=font_color)
+        net.from_nx(self.graph)
+        net.set_options("""
+        var options = {
+        "physics": {
+            "enabled": true,
+            "stabilization": {
+            "enabled": true,
+            "iterations": 500
+            },
+            "barnesHut": {      
+            "springLength": 200,
+            "springConstant": 0.02,
+            "damping": 0.3
+            }
+        }
+        }
+        """)
+        # Save and show
+        net.show("research_network.html")
 
 # pip install pyvis
 import networkx as nx
 from pyvis.network import Network
+net=Scholarnet(dblp_url="https://dblp.org/pid/290/7993.html")
+net=Scholarnet()
+net.extract_graph()
+net.show()
 
-# Create Pyvis network
-net = Network(notebook=True, height="600px", width="100%", bgcolor="#222222", font_color="white")
-net.from_nx(G)
-
-net.set_options("""
-var options = {
-  "physics": {
-    "enabled": true,
-    "stabilization": {
-      "enabled": true,
-      "iterations": 500
-    },
-    "barnesHut": {      
-      "springLength": 200,
-      "springConstant": 0.02,
-      "damping": 0.3
-    }
-  }
-}
-""")
-
-
-
-# Save and show
-net.show("research_network.html")
